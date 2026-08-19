21988f1c3974d68437615871c1a85c446d1bf706
Your GitHub Username <sandhyadurairaj02@gmail.com>
2026-08-19 23:03:17 +0530

    Fix CORS URL syntax

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://document-qa-chatbot-2.vercel.app",
-        https://document-qa-chatbot-1mxx.vercel.app/
+        "https://document-qa-chatbot-1mxx.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
